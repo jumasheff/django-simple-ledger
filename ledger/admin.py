b6d73a91397f077ffadcccdcaef1e4ef3a73b45b
murat <jumasheff@gmail.com>
2015-01-06 12:02:25 +0600

    Added custom clinic filter

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,12 +1,38 @@
 from django.contrib import admin
+from django.contrib.admin import SimpleListFilter
+from django.contrib.contenttypes.models import ContentType
+from django.db.models import Q
 from ledger.models import Transaction
+from main.models import Clinic
 from django.utils.translation import ugettext_lazy as _
+
+
+class ClinicTransactionFilter(SimpleListFilter):
+    # Human-readable title which will be displayed in the
+    # right admin sidebar just above the filter options.
+    title = _('Clinic')
+
+    # Parameter for the filter that will be used in the URL query.
+    parameter_name = 'clinic'
+    def lookups(self, request, model_admin):
+        clinics = [(o['pk'], o['name']) for o in Clinic.objects.all().values('pk', 'name')]
+        return clinics
+    def queryset(self, request, queryset):
+        try:
+            clinic = Clinic.objects.get(pk=self.value())
+            return queryset.filter(Q(agent_from_id=clinic.pk,
+                                   agent_from_content_type=ContentType.objects.get_for_model(clinic)) | Q(agent_to_id=clinic.pk,
+                                   agent_to_content_type=ContentType.objects.get_for_model(clinic)))
+        except Clinic.DoesNotExist:
+            pass
 
 
 class TransactionAdmin(admin.ModelAdmin):
     list_display = ['agent_from', 'agent_to', 'amount', 'batch_id', 'transaction_type', 'reason', 'from_deposit',
                     'date_created']
-    search_fields = ['agent_from', 'agent_to']
+    list_filter = (ClinicTransactionFilter,)
+
+
 
     def agent_from(self, instance):
         return instance.agent_from
@@ -25,6 +51,5 @@ class TransactionAdmin(admin.ModelAdmin):
 
     class Meta:
         model = Transaction
-
 
 admin.site.register(Transaction, TransactionAdmin)
